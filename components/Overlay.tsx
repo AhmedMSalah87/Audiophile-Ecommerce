c0d43cd299aa2f293a555421c9f816f125c93563
AhmedMSalah87 <ahmed_salah45@hotmail.com>
2025-05-01 20:31:02 +0300

    change height of overlay

--- components/Overlay.tsx
+++ components/Overlay.tsx
@@ -1,23 +1,35 @@
 "use client"; // must for overlay component as context hook works only in client component
 import { useOverlay } from "@/hooks/useOverlay";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 const Overlay = () => {
   const { isOverlayVisible, setIsOverlayVisible } = useOverlay();
+  const overlayRef = useRef<HTMLDivElement | null>(null);
   const pathname = usePathname();
   const overlay: React.CSSProperties = {
     position: "absolute",
     top: "96px" /* Offset for header height*/,
     left: "0",
     width: "100%",
-    height: "calc(100vh - 96px)" /* Subtract header height */,
+    // height: "calc(100vh - 96px)" /* Subtract header height */,
     background: "rgba(0, 0, 0, 0.5)" /* Semi-transparent */,
     zIndex: "99",
   };
   // to ensure overlay is closed when page refreshed or navigation changed
   useEffect(() => setIsOverlayVisible(false), [pathname]);
 
-  return isOverlayVisible && <div style={overlay}></div>;
+  // for making overlay cover the all page height
+  useEffect(() => {
+    const setOverlayHeight = () => {
+      if (overlayRef.current) {
+        overlayRef.current.style.height =
+          document.documentElement.scrollHeight + "px";
+      }
+    };
+    setOverlayHeight();
+  }, [isOverlayVisible]);
+
+  return isOverlayVisible && <div style={overlay} ref={overlayRef}></div>;
 };
 
 export default Overlay;
